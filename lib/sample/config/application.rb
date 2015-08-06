caeb69018d40e5ccf9016e4d27215e71f8315a3f
fushang318 <fushang318@gmail.com>
2015-08-06 17:39:42 +0800

    增加测试相关配置

--- lib/sample/config/application.rb
+++ lib/sample/config/application.rb
@@ -9,7 +9,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -17,6 +17,10 @@ Bundler.require(*Rails.groups)
 
 module Sample
   class Application < Rails::Application
+    config.generators do |g|
+      g.factory_girl dir: 'spec/factories'
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
