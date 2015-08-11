e8859902f02b25cb617b5ea7e133f5310ccd77b4
ben7th <ben7th@sina.com>
2015-08-11 18:35:12 +0800

    增加 web console 配置

--- lib/sample/config/environments/development.rb
+++ lib/sample/config/environments/development.rb
@@ -36,5 +36,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
+  BetterErrors::Middleware.allow_ip! '0.0.0.0/0'
+  config.web_console.whitelisted_ips = '0.0.0.0/0'
 end
